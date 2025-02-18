fc3b3a7b0414a35a4cec05579412a3b69cde8288
Calvin Kamp <161054857+calvin-kamp@users.noreply.github.com>
2025-02-18 19:43:57 +0100

    added npm package for robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import mkcert from 'vite-plugin-mkcert';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+import robotsConfig from './robots-txt.config';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://astro-starter-kit.calvin-kamp.com',
     vite: {
         plugins: [mkcert()],
         server: {
-            https: true
+            https: true,
         },
     },
+    integrations: [sitemap(), robotsTxt(robotsConfig)]
 });
