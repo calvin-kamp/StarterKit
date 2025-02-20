a0cd2a262deab4856dc95999d07e6e145ba2ed8f
Calvin Kamp <161054857+calvin-kamp@users.noreply.github.com>
2025-02-20 11:57:29 +0100

    added alpine.js and example component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import mkcert from 'vite-plugin-mkcert';
+import alpine from '@astrojs/alpinejs';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import robotsConfig from './robots-txt.config';
@@ -14,5 +15,5 @@ export default defineConfig({
             https: true,
         },
     },
-    integrations: [sitemap(), robotsTxt(robotsConfig)]
+    integrations: [alpine(), sitemap(), robotsTxt(robotsConfig)]
 });
